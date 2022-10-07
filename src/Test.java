e92bd6e4d84708b4cb6c36ba3f20e1f822c593b8
El_nino <null>
2022-10-07 21:22:28 +0800

    java与命令行交互，我似乎没必要写一个.bat文件作为整个项目的开始了

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,56 @@
+import java.io.*;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hell");
+
+    }
+
+    @org.junit.Test
+    public void callCommandLine() {
+        Runtime runtime = Runtime.getRuntime();
+
+        try {
+            //查询文件的md5码
+            String command = "certutil -hashfile D:\\CODES(daima)\\personal-DB\\src\\createTables.sql md5";
+            Process process = runtime.exec(command);
+            InputStream inputStream = process.getInputStream();
+            // windows命令行使用gbk编码，需要转换
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "gbk"));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @org.junit.Test
+    public void waitForCommandLine() {
+        Runtime runtime = Runtime.getRuntime();
+
+        try {
+            String command = "echo hello, world";
+            Process process = runtime.exec(command);
+
+            //等待process代表的进程结束
+            process.waitFor();
+
+            System.out.println("command line process terminated");
+
+            System.out.println();
+
+            InputStream inputStream = process.getInputStream();
+            // windows命令行使用gbk编码，需要转换
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "gbk"));
+
+            System.out.println(reader.readLine());
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
