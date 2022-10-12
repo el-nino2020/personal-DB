5b16a0fc1f3d91eaf20074739f85a958ac484699
El_nino <null>
2022-10-12 18:56:44 +0800

    java bean for record

---
+++ src/domain/FileInfo.java
@@ -0,0 +1,76 @@
+package domain;
+
+import java.time.LocalDateTime;
+
+public class FileInfo {
+    private int id;
+    private String filename;
+    private LocalDateTime lastmodified;
+    private String passwd;
+    private String md5value;
+    private String note;
+
+
+    public FileInfo() {
+    }
+
+    @Override
+    public String toString() {
+        return "FileInfo{" +
+                "id=" + id +
+                ", filename='" + filename + '\'' +
+                ", lastmodified=" + lastmodified +
+                ", passwd='" + passwd + '\'' +
+                ", md5value='" + md5value + '\'' +
+                ", note='" + note + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public LocalDateTime getLastmodified() {
+        return lastmodified;
+    }
+
+    public void setLastmodified(LocalDateTime lastmodified) {
+        this.lastmodified = lastmodified;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getMd5value() {
+        return md5value;
+    }
+
+    public void setMd5value(String md5value) {
+        this.md5value = md5value;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
