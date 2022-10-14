88e5ad47b13e603258065e8da8345abdf04a6830
El_nino <null>
2022-10-14 16:58:22 +0800

    add two service classes

---
+++ src/service/ArchiveService.java
@@ -0,0 +1,8 @@
+package service;
+
+
+/**
+ * 负责压缩包相关的服务
+ */
+public class ArchiveService {
+}
---
+++ src/service/DBService.java
@@ -0,0 +1,7 @@
+package service;
+
+/**
+ * 负责数据库的备份和已有表的查询，也许还要负责新建表？
+ */
+public class DBService {
+}
