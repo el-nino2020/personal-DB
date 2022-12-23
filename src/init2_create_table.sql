1e8dd7a9303486633b92b5f9ded064d0f32f5503
El_nino <null>
2022-12-23 19:17:27 +0800

    使用sqlyog创建表时的SQL preview功能生成的规范的Create table语句

--- src/init2_create_table.sql
+++ src/init2_create_table.sql
@@ -2,27 +2,33 @@ CREATE DATABASE cloud_backup;
 USE cloud_backup;
 
 -- store the information of directories/folders
-CREATE TABLE directories(
-	dirname VARCHAR(255) UNIQUE NOT NULL, -- unqiue will also create index
-	note VARCHAR(255), -- some comments about the directory, if any
-	id INT PRIMARY KEY AUTO_INCREMENT
-)ENGINE=INNODB;
+CREATE TABLE `cloud_backup`.`directories` (
+    `dir_name` VARCHAR (255) NOT NULL,
+    `dir_note` VARCHAR (500) COMMENT 'some comments about this directory, if any',
+    `dir_id` INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (`dir_id`),
+    UNIQUE INDEX `UNIQUE_DIR_NAME` (`dir_name`)
+) ENGINE = INNODB;
+
+
 
 
 -- store the information of files
-CREATE TABLE files(
-	filename VARCHAR(255) NOT NULL,
-	note VARCHAR(255), -- some comments about the file, if any
-	filesize BIGINT NOT NULL, -- file size in bytes
-	lastmodified DATETIME NOT NULL,
-	passwd VARCHAR(255) NOT NULL,
-	md5value char(32) NOT NULL,
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dirid INT NOT NULL , -- directories.id
-	FOREIGN KEY (dirid) REFERENCES directories(id)
-)ENGINE=INNODB;
+CREATE TABLE `cloud_backup`.`files` (
+    `file_name` VARCHAR (255) NOT NULL,
+    `file_note` VARCHAR (500) COMMENT 'some comments about this file, if any',
+    `size` BIGINT NOT NULL COMMENT 'file size in bytes',
+    `last_modified` DATETIME NOT NULL COMMENT 'last modified time',
+    `password` VARCHAR (255) NOT NULL COMMENT 'archive password',
+    `md5_value` CHAR (32) NOT NULL COMMENT 'md5(archive)',
+    `file_id` INT NOT NULL AUTO_INCREMENT,
+    `dir_id` INT NOT NULL,
+    PRIMARY KEY (`file_id`),
+    INDEX `FILE_NAME_INDEX` (`file_name`) COMMENT "for fast query",
+    CONSTRAINT `FOREIGN_DIR_ID` FOREIGN KEY (`dir_id`) REFERENCES `cloud_backup`.`directories` (`dir_id`)
+) ENGINE = INNODB;
 
-CREATE INDEX filenameIndex ON files(filename);
+
 
 -- change 'newTale' to other (table/folder) name
 -- CREATE TABLE newTable LIKE template_table;
